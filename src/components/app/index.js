3e23a82d4fe19214d7dd99031a2f9cab3f9968f8
nickjaz <david@vicklund.com>
2019-03-09 17:23:28 -0800

    cleans up react app

---
+++ src/components/app/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app_container">
+        <h1>This works</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
